9592caa5ef5704b8cd3445cb73e9048ca6ea38de
Kjetil Valle <kjetil.valle@gmail.com>
2025-05-13 21:27:37 +0200

    Control buttons using Automaton

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,43 @@
 #include <Arduino.h>
+#include <Automaton.h>
 #include "Buzzer.h"
 #include "Sounds.h"
 
 Buzzer buzzer1(D1);
 Buzzer buzzer2(D2);
 
-#define BUTTON_PIN_1 D0
-#define BUTTON_PIN_2 D5
-#define BUTTON_PIN_3 D6
-#define BUTTON_PIN_4 D7
+Atm_button button1;
+Atm_button button2;
+Atm_button button3;
+Atm_button button4;
 
-int buttonState1 = 0;
-int buttonState2 = 0;
-int buttonState3 = 0;
-int buttonState4 = 0;
+void handleButtonPress(int idx, int v, int up)
+{
+  if (v)
+  {
+    Serial.println("Button pressed: " + String(idx));
+    if (idx == 1)
+    {
+      Serial.println("Button 1 pressed");
+      buzzer1.play(marioCoin);
+    }
+    else if (idx == 2)
+    {
+      Serial.println("Button 2 pressed");
+      buzzer1.play(marioPowerUp);
+    }
+    else if (idx == 3)
+    {
+      Serial.println("Button 3 pressed");
+      buzzer1.play(marioOneUp);
+    }
+    else if (idx == 4)
+    {
+      Serial.println("Button 4 pressed");
+      buzzer1.play(marioGameOver);
+    }
+  }
+}
 
 void setup()
 {
@@ -21,48 +45,24 @@ void setup()
   delay(1000);
   Serial.println("\nSerial communication started.");
 
-  pinMode(BUTTON_PIN_1, INPUT_PULLUP);
-  pinMode(BUTTON_PIN_2, INPUT_PULLUP);
-  pinMode(BUTTON_PIN_3, INPUT_PULLUP);
-  pinMode(BUTTON_PIN_4, INPUT_PULLUP);
-
   buzzer1.setup();
   buzzer2.setup();
+
+  button1.begin(D0)
+      .onPress(handleButtonPress, 1);
+  button2.begin(D5)
+      .onPress(handleButtonPress, 2);
+  button3.begin(D6)
+      .onPress(handleButtonPress, 3);
+  button4.begin(D7)
+      .onPress(handleButtonPress, 4);
 
   buzzer1.play(fanfare);
 }
 
 void loop()
 {
-  buttonState1 = digitalRead(BUTTON_PIN_1);
-  buttonState2 = digitalRead(BUTTON_PIN_2);
-  buttonState3 = digitalRead(BUTTON_PIN_3);
-  buttonState4 = digitalRead(BUTTON_PIN_4);
-
-  if (buttonState1 == HIGH)
-  {
-    Serial.println("Button pressed");
-    buzzer1.play(buttonPressDouble);
-  }
-
-  if (buttonState2 == HIGH)
-  {
-    Serial.println("Button pressed");
-    buzzer1.play(buttonPressDouble);
-  }
-
-  if (buttonState3 == HIGH)
-  {
-    Serial.println("Button pressed");
-    buzzer1.play(buttonPressDouble);
-  }
-
-  if (buttonState4 == HIGH)
-  {
-    Serial.println("Button pressed");
-    buzzer1.play(buttonPressDouble);
-  }
-
+  automaton.run();
   buzzer1.update();
   buzzer2.update();
 }
